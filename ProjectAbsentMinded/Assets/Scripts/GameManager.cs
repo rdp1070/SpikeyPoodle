7c5e740a921049f14f8cf8d85e1e2c6ef21d8760
Robert Pruden <rdp1070@rit.edu>
2019-10-06 16:24:18 -0400

    resolves #15 Adds crouching to the game and it's effects

--- ProjectAbsentMinded/Assets/Scripts/GameManager.cs
+++ ProjectAbsentMinded/Assets/Scripts/GameManager.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
@@ -7,17 +8,21 @@ public class GameManager : MonoBehaviour
 {
     private readonly bool isThieving;
 
-    public GameObject player;
+    public Player player;
     public ScoreZone scoreZone;
     public BaseItem winItem;
     public int numItems;
     public List<BaseItem> allItems = new List<BaseItem>();
+    public PlayerDetection[] playerDetectors;
+    public int visionReduction = 7;
 
     public bool IsThieving { get => CheckPlayerHasItem(); }
 
 
     private void Start()
     {
+        playerDetectors = GetComponentsInChildren<PlayerDetection>();
+
         if (scoreZone != null)
         {
             scoreZone.itemEnterScoreZone.AddListener(CheckWin);
@@ -26,8 +31,31 @@ public class GameManager : MonoBehaviour
             var newItem = GenerateItem();
             // TODO figure out how to not spawn them on top of each other
         }
+
+        player.crouched.AddListener(ReduceVision);
+        player.stand.AddListener(ReturnVision);
     }
 
+    /// <summary>
+    /// Go through all the player detectors and increase their vision by some fraction
+    /// </summary>
+    private void ReturnVision()
+    {
+        foreach (PlayerDetection detector in playerDetectors) {
+            detector.viewDistance += visionReduction;
+        }
+    }
+
+    /// <summary>
+    /// Go through all the player detectors and reduce their vision by some fraction
+    /// </summary>
+    private void ReduceVision()
+    {
+        foreach (PlayerDetection detector in playerDetectors)
+        {
+            detector.viewDistance -= visionReduction;
+        }
+    }
 
     bool CheckPlayerHasItem()
     {
@@ -56,10 +84,10 @@ public class GameManager : MonoBehaviour
     {
         BaseItem newItem;
         newItem = Instantiate<BaseItem>(winItem);
-        newItem.color = BaseItem.ATTR.COLOR.colors[Random.Range(0, BaseItem.ATTR.COLOR.colors.Count)];
-        newItem.size = BaseItem.ATTR.SIZE.sizes[Random.Range(0, BaseItem.ATTR.SIZE.sizes.Count)];
-        newItem.shape = BaseItem.ATTR.SHAPE.shapes[Random.Range(0, BaseItem.ATTR.SHAPE.shapes.Count)];
-        newItem.weight = BaseItem.ATTR.WEIGHT.weights[Random.Range(0, BaseItem.ATTR.WEIGHT.weights.Count)];
+        newItem.color = BaseItem.ATTR.COLOR.colors[UnityEngine.Random.Range(0, BaseItem.ATTR.COLOR.colors.Count)];
+        newItem.size = BaseItem.ATTR.SIZE.sizes[UnityEngine.Random.Range(0, BaseItem.ATTR.SIZE.sizes.Count)];
+        newItem.shape = BaseItem.ATTR.SHAPE.shapes[UnityEngine.Random.Range(0, BaseItem.ATTR.SHAPE.shapes.Count)];
+        newItem.weight = BaseItem.ATTR.WEIGHT.weights[UnityEngine.Random.Range(0, BaseItem.ATTR.WEIGHT.weights.Count)];
         return newItem;
     }
 }
--- ProjectAbsentMinded/Assets/Scripts/Player.cs
+++ ProjectAbsentMinded/Assets/Scripts/Player.cs
@@ -2,20 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityStandardAssets.Characters.FirstPerson;
+using UnityEngine.Events;
 
 public class Player : MonoBehaviour
 {
     public PickupItem pickupItem;
     public FirstPersonController firstPersonController;
+    public GameObject playerBody;
     public float ogWalkSpeed;
     public float ogRunSpeed;
     public float ogMouseLookSpeed;
+    public bool isCrouching = false;
+    private float speedModifier = .9f;
+    public UnityEvent crouched = new UnityEvent();
+    public UnityEvent stand = new UnityEvent();
+
+
 
     private void Start()
     {
         pickupItem = GetComponentInChildren<PickupItem>();
         firstPersonController = GetComponentInChildren<FirstPersonController>();
         if (firstPersonController != null) {
+            playerBody = firstPersonController.gameObject;
             ogWalkSpeed = firstPersonController.m_WalkSpeed; // equal to original walk speed.
             ogRunSpeed = firstPersonController.m_RunSpeed;
             ogMouseLookSpeed = firstPersonController.m_MouseLook.XSensitivity;
@@ -25,21 +34,53 @@ public class Player : MonoBehaviour
     private void Update()
     {
         ApplyWeight();
+        if (Input.GetKeyUp(KeyCode.C)) {
+            if (isCrouching)
+                StopCrouch();
+            else
+                Crouch();
+        }
+        if (Input.GetKeyDown(KeyCode.LeftShift) && isCrouching) {
+            StopCrouch();
+        }
+    }
+
+    private void Crouch()
+    {
+        isCrouching = true;
+        crouched.Invoke();
+        if (playerBody != null) {
+            playerBody.transform.localScale = new Vector3(1f, .75f, 1f);
+        }
+    }
+
+    private void StopCrouch() {
+        isCrouching = false;
+        stand.Invoke();
+        if (playerBody != null)
+        { 
+            playerBody.transform.localScale = new Vector3(1f, 1f, 1f);
+        }
     }
 
     private void ApplyWeight()
     {
+        var mod = 1f;
+        if (isCrouching)
+            mod = speedModifier;
+
         if (pickupItem.heldItem != null)
         {
-            firstPersonController.m_RunSpeed = ogRunSpeed * pickupItem.heldItem.weight;
-            firstPersonController.m_WalkSpeed = ogWalkSpeed * pickupItem.heldItem.weight;
+            
+            firstPersonController.m_RunSpeed = (ogRunSpeed * pickupItem.heldItem.weight) * mod;
+            firstPersonController.m_WalkSpeed = (ogWalkSpeed * pickupItem.heldItem.weight) * mod;
             firstPersonController.m_MouseLook.XSensitivity = ogMouseLookSpeed * pickupItem.heldItem.weight;
             firstPersonController.m_MouseLook.YSensitivity = ogMouseLookSpeed * pickupItem.heldItem.weight;
         }
         else
         {
-            firstPersonController.m_RunSpeed = ogRunSpeed;
-            firstPersonController.m_WalkSpeed = ogWalkSpeed;
+            firstPersonController.m_RunSpeed = ogRunSpeed * mod;
+            firstPersonController.m_WalkSpeed = ogWalkSpeed * mod;
             firstPersonController.m_MouseLook.XSensitivity = ogMouseLookSpeed;
             firstPersonController.m_MouseLook.YSensitivity = ogMouseLookSpeed;
         }
