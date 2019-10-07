6f717e23e55ba92bc3361ef1d813f705960adf2c
Robert Pruden <rdp1070@rit.edu>
2019-10-06 23:34:39 -0400

    resolves #17 Add muzak speaker prefab, and looper for when intro is done

---
+++ ProjectAbsentMinded/Assets/AudioLooper.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioLooper : MonoBehaviour
+{
+    public AudioSource audioSourceIntro;
+    public AudioSource audioSourceLoop;
+    private bool startedLoop;
+
+    void FixedUpdate()
+    {
+        if (!audioSourceIntro.isPlaying && !startedLoop)
+        {
+            audioSourceLoop.Play();
+            Debug.Log("Done playing");
+            startedLoop = true;
+        }
+    }
+}
